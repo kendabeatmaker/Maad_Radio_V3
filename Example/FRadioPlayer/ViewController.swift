f0a86824c3830520958412bee2e3edabc0ca1536
Ken Taylor <kendabeatmaker@gmail.com>
2020-05-15 22:37:43 -0400

    h

--- Example/FRadioPlayer/ViewController.swift
+++ Example/FRadioPlayer/ViewController.swift
@@ -26,43 +26,43 @@ class ViewController: UIViewController {
     let player: FRadioPlayer = FRadioPlayer.shared
     
     // List of stations
-    let stations = [Station(name: "Newport Folk Radio",
-                            detail: "Are you ready to Folk?",
-                            url: URL(string: "https://streaming.broadcastradio.com:8872/maad975fm")!,
-                            image: #imageLiteral(resourceName: "station2")),
-                    
-                    Station(name: "Absolute Country Hits Radio",
-                             detail: "The Music Starts Here",
-                             url: URL(string: "http://strm112.1.fm/acountry_mobile_mp3")!,
-                             image: #imageLiteral(resourceName: "station1")),
-                                          
-                     Station(name: "The Alt Vault",
-                             detail: "Your Lifestyle... Your Music!",
-                             url: URL(string: "http://jupiter.prostreaming.net/altmixxlow")!,
-                             image: #imageLiteral(resourceName: "station3"))]
-    
+//    let stations = [Station(name: "Newport Folk Radio",
+//                            detail: "Are you ready to Folk?",
+//                            url: URL(string: "https://streaming.broadcastradio.com:8872/maad975fm")!,
+//                            image: #imageLiteral(resourceName: "station2")),
+//
+//                    Station(name: "Absolute Country Hits Radio",
+//                             detail: "The Music Starts Here",
+//                             url: URL(string: "http://strm112.1.fm/acountry_mobile_mp3")!,
+//                             image: #imageLiteral(resourceName: "station1")),
+//
+//                     Station(name: "The Alt Vault",
+//                             detail: "Your Lifestyle... Your Music!",
+//                             url: URL(string: "http://jupiter.prostreaming.net/altmixxlow")!,
+//                             image: #imageLiteral(resourceName: "station3"))]
+//
     // Selected station index
-    var selectedIndex = 0 {
-        didSet {
-            defer {
-                selectStation(at: selectedIndex)
-                updateNowPlaying(with: track)
-            }
-            
-            guard 0..<stations.endIndex ~= selectedIndex else {
-                selectedIndex = selectedIndex < 0 ? stations.count - 1 : 0
-                return
-            }
-        }
-    }
+//    var selectedIndex = 0 {
+//        didSet {
+//            defer {
+//                selectStation(at: selectedIndex)
+//                updateNowPlaying(with: track)
+//            }
+//
+//            guard 0..<stations.endIndex ~= selectedIndex else {
+//                selectedIndex = selectedIndex < 0 ? stations.count - 1 : 0
+//                return
+//            }
+//        }
+//    }
     
-    var track: Track? {
-        didSet {
-            artistLabel.text = track?.artist
-            trackLabel.text = track?.name
-            updateNowPlaying(with: track)
-        }
-    }
+//    var track: Track? {
+//        didSet {
+//            artistLabel.text = track?.artist
+//            trackLabel.text = track?.name
+//            updateNowPlaying(with: track)
+//        }
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -81,7 +81,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func playTap(_ sender: Any) {
-        player.radioURL = URL(string: "https://streaming.broadcastradio.com:8872/maad975fm")
+        player.radioURL = URL(string: "http://streaming.broadcastradio.com:8870/maad975fm")
         player.togglePlaying()
     }
     
@@ -89,22 +89,22 @@ class ViewController: UIViewController {
         player.stop()
     }
     
-    @IBAction func previousTap(_ sender: Any) {
-        previous()
-    }
+//    @IBAction func previousTap(_ sender: Any) {
+//        previous()
+//    }
     
-    @IBAction func nextTap(_ sender: Any) {
-        next()
-    }
-    
-    func next() {
-        selectedIndex += 1
-    }
-    
-    func previous() {
-        selectedIndex -= 1
-    }
-    
+//    @IBAction func nextTap(_ sender: Any) {
+//        next()
+//    }
+//
+//    func next() {
+//        selectedIndex += 1
+//    }
+//
+//    func previous() {
+//        selectedIndex -= 1
+//    }
+//
     func selectStation(at position: Int) {
         player.radioURL = URL(string: "https://streaming.broadcastradio.com:8872/maad975fm")
         //tableView.selectRow(at: IndexPath(item: position, section: 0), animated: true, scrollPosition: .none)
@@ -123,51 +123,51 @@ extension ViewController: FRadioPlayerDelegate {
         playButton.isSelected = player.isPlaying
     }
     
-    func radioPlayer(_ player: FRadioPlayer, metadataDidChange artistName: String?, trackName: String?) {
-        track = Track(artist: artistName, name: trackName)
-    }
+//    func radioPlayer(_ player: FRadioPlayer, metadataDidChange artistName: String?, trackName: String?) {
+//        track = Track(artist: artistName, name: trackName)
+//    }
+//
+//    func radioPlayer(_ player: FRadioPlayer, itemDidChange url: URL?) {
+//        track = nil
+//    }
     
-    func radioPlayer(_ player: FRadioPlayer, itemDidChange url: URL?) {
-        track = nil
-    }
+//    func radioPlayer(_ player: FRadioPlayer, metadataDidChange rawValue: String?) {
+//        infoContainer.isHidden = (rawValue == nil)
+//    }
     
-    func radioPlayer(_ player: FRadioPlayer, metadataDidChange rawValue: String?) {
-        infoContainer.isHidden = (rawValue == nil)
-    }
-    
-    func radioPlayer(_ player: FRadioPlayer, artworkDidChange artworkURL: URL?) {
-        
-        // Please note that the following example is for demonstration purposes only, consider using asynchronous network calls to set the image from a URL.
-        guard let artworkURL = artworkURL, let data = try? Data(contentsOf: artworkURL) else {
-            artworkImageView.image = stations[selectedIndex].image
-            return
-        }
-        track?.image = UIImage(data: data)
-        artworkImageView.image = track?.image
-        updateNowPlaying(with: track)
-    }
+//    func radioPlayer(_ player: FRadioPlayer, artworkDidChange artworkURL: URL?) {
+//
+//        // Please note that the following example is for demonstration purposes only, consider using asynchronous network calls to set the image from a URL.
+//        guard let artworkURL = artworkURL, let data = try? Data(contentsOf: artworkURL) else {
+//            artworkImageView.image = stations[selectedIndex].image
+//            return
+//        }
+//        track?.image = UIImage(data: data)
+//        artworkImageView.image = track?.image
+//        updateNowPlaying(with: track)
+//    }
 }
 
 // MARK: - UITableViewDelegate
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return stations.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = stations[indexPath.item].name
-        cell.detailTextLabel?.text = stations[indexPath.item].detail
-        cell.imageView?.image = stations[indexPath.item].image
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selectedIndex = indexPath.item
-    }
-}
+//extension ViewController: UITableViewDelegate, UITableViewDataSource {
+//    
+////    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+////        return stations.count
+////    }
+//    
+////    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+////        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+////        cell.textLabel?.text = stations[indexPath.item].name
+////        cell.detailTextLabel?.text = stations[indexPath.item].detail
+////        cell.imageView?.image = stations[indexPath.item].image
+////        return cell
+////    }
+//    
+////    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+////        selectedIndex = indexPath.item
+////    }
+//}
 
 // MARK: - Remote Controls / Lock screen
 
@@ -179,7 +179,9 @@ extension ViewController {
         
         // Add handler for Play Command
         commandCenter.playCommand.addTarget { [unowned self] event in
+            
             if self.player.rate == 0.0 {
+                self.player.volume = 0.5
                 self.player.play()
                 return .success
             }
@@ -196,16 +198,16 @@ extension ViewController {
         }
         
         // Add handler for Next Command
-        commandCenter.nextTrackCommand.addTarget { [unowned self] event in
-            self.next()
-            return .success
-        }
+//        commandCenter.nextTrackCommand.addTarget { [unowned self] event in
+//            self.next()
+//            return .success
+//        }
         
         // Add handler for Previous Command
-        commandCenter.previousTrackCommand.addTarget { [unowned self] event in
-            self.previous()
-            return .success
-        }
+//        commandCenter.previousTrackCommand.addTarget { [unowned self] event in
+//            self.previous()
+//            return .success
+//        }
     }
     
     func updateNowPlaying(with track: Track?) {
@@ -217,13 +219,13 @@ extension ViewController {
             nowPlayingInfo[MPMediaItemPropertyArtist] = artist
         }
         
-        nowPlayingInfo[MPMediaItemPropertyTitle] = track?.name ?? stations[selectedIndex].name
+        //nowPlayingInfo[MPMediaItemPropertyTitle] = track?.name ?? stations[selectedIndex].name
         
-        if let image = track?.image ?? stations[selectedIndex].image {
-            nowPlayingInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(boundsSize: image.size, requestHandler: { _ -> UIImage in
-                return image
-            })
-        }
+//        if let image = track?.image ?? stations[selectedIndex].image {
+//            nowPlayingInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(boundsSize: image.size, requestHandler: { _ -> UIImage in
+//                return image
+//            })
+//        }
         
         // Set the metadata
         MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
