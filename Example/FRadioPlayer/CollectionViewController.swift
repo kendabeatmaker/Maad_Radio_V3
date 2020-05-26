9dbde534f7a02ac20fbdf3194eea6609c4844666
Ken Taylor <kendabeatmaker@gmail.com>
2020-05-25 22:20:18 -0400

    Almost Perfect

--- Example/FRadioPlayer/CollectionViewController.swift
+++ Example/FRadioPlayer/CollectionViewController.swift
@@ -10,14 +10,77 @@ import UIKit
 import SDWebImage
 
 class CollectionViewController: UIViewController {
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var collectionViewFlowLayout: UICollectionViewFlowLayout!
+    
+    let cellIdentifier = "CustomCollectionViewCell"
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCollectionView()
 
-        
-        
-        
     }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setupCollectionViewItemZize()
+    }
+    
+    
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        let nib = UINib(nibName: "CustomCollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: cellIdentifier)
+    }
+    
+    private func setupCollectionViewItemZize() {
+        if collectionViewFlowLayout == nil {
+            let numberOfItemPerRow: CGFloat = 3
+            let lineSpacing: CGFloat = 5
+            let interItemSpacing: CGFloat = 5
+            let width = (collectionView.frame.width - (numberOfItemPerRow - 1) * interItemSpacing) / numberOfItemPerRow
+            let height = width
+            
+            collectionViewFlowLayout = UICollectionViewFlowLayout()
+            
+            collectionViewFlowLayout.itemSize = CGSize(width: width, height: height)
+            collectionViewFlowLayout.sectionInset = .zero
+            collectionViewFlowLayout.scrollDirection = .vertical
+            collectionViewFlowLayout.minimumLineSpacing = lineSpacing
+            collectionViewFlowLayout.minimumInteritemSpacing = interItemSpacing
+            collectionView.setCollectionViewLayout(collectionViewFlowLayout, animated: true)
+            
+        }
+    }
+    
+    
+}
 
+extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        NetworkStuff.urlArrayReturnCount
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! CustomCollectionViewCell
+        cell.imageView?.sd_setImage(with: NetworkStuff.urlArrayReturn[indexPath.row], placeholderImage: #imageLiteral(resourceName: "placeholderImage"), options: .highPriority, completed: nil)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Item clicked at \(indexPath)")
+    }
+    
+    
+    
+    
 }
