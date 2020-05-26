81e502b11ba131d91b8dc0db214d6c2f7d9635d6
Ken Taylor <kendabeatmaker@gmail.com>
2020-05-25 21:03:04 -0400

    starting Collection View, i really had to learn tableview first i didn't mind... :(

---
+++ Example/FRadioPlayer/CollectionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewController.swift
+//  FRadioPlayer_ios
+//
+//  Created by Ken Taylor on 25/05/2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+class CollectionViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+    }
+
+}
---
+++ Example/FRadioPlayer/CustomCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomCollectionViewCell.swift
+//  FRadioPlayer_ios
+//
+//  Created by Ken Taylor on 25/05/2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
